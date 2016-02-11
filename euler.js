098294e8654fa73a949aa3e5c9a005bb96cc7fae
Brett Hansen <hansen.brett7@gmail.com>
2016-02-10 22:39:44 -0700

    Now drawing triangle vertices with absolute and relative coordinate system.

--- euler.js
+++ euler.js
@@ -12,29 +12,51 @@ var color_green = [0, 255, 0, 255];
 var color_blue  = [0, 0, 255, 255];
 
 // Runtime Variables
-// var triangleCoords = 
+var triangleCoords = [[0, 1], [1, -.5], [-1, -.5]];
+var x_bounds = [-1.5, 1.5];
+var y_bounds = [-1.5, 1.5];
+// Must be update together with bounds
+var x_width = 3.0;
+var y_height = 3.0;
 
 function initialize() {
   canvas = $('#drawingCanvas')[0];
+  canvas.style.background = "#EDEDED";
   context = canvas.getContext("2d");
   drawCanvas();
 }
 initialize();
 
 function drawCanvas() {
-  image = context.createImageData($(canvas).innerWidth() - 20, $(canvas).innerHeight() - 60);
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-  for(var i = 0; i < image.height; i++) {
-  	for(var j = 0; j < image.width; j++) {
-        image.data.set(color_grey, 4 * (i * image.width + j));
-  	}
-  }
-  context.putImageData(image, 0, 0);
+  drawTriangeVertices();
 }
 
+function drawTriangeVertices() {
+  context.fillStyle = "#90C3D4";
+  for(var i = 0; i < triangleCoords.length; i++) {
+    var coords = convertAbsToRelPoint(triangleCoords[i]);
+    context.arc(coords[0], coords[1], 5, 0, 2 * Math.PI);
+    context.fill();
+    context.closePath();
+  }
+}
 
-function resize() {
-    $('#drawingCanvas')[0].width = $(canvas).innerWidth() - 20;
-    $('#drawingCanvas')[0].height = $(canvas).innerHeight() - 60;
-    drawCanvas();
+function convertAbsToRelPoint(point) {
+  var x = point[0] / x_width;
+  var y = point[1] / y_height;
+
+  x += 0.5;
+  y += 0.5;
+
+  y = 1.0 - y;
+
+  x *= 1.0 * canvas.width;
+  y *= 1.0 * canvas.height;
+
+  x = Math.floor(x);
+  y = Math.floor(y);
+
+  return [x, y];
 }
