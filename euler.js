27e2092835fc13429b9aa8d55295d4d8fc8d09b6
Brett Hansen <hansen.brett7@gmail.com>
2016-02-12 23:43:04 -0700

    All centers implemented.

--- euler.js
+++ euler.js
@@ -19,18 +19,22 @@ var color_red   = [255, 0, 0, 255];
 var color_green = [0, 255, 0, 255];
 var color_blue  = [0, 0, 255, 255];
 
+var vertRadius = 8;
 var nodeRadius = 5;
+var tolerance = 2;
 
 // Runtime Variables
-var triangleCoords = new Object();
-triangleCoords.a = new Point(0, 1.0);
-triangleCoords.b = new Point(1, -.5);
-triangleCoords.c = new Point(-1, -.5);
+var verts = new Object();
+verts.a = new Point(0, 1.0);
+verts.b = new Point(1, -.5);
+verts.c = new Point(-1, -.5);
 var x_bounds = new Point(-1.5, 1.5);
 var y_bounds = new Point(-1.5, 1.5);
 var x_width = 3.0;
 var y_height = 3.0;
 var orthoCenter = new Point();
+var circmCenter = new Point();
+var cntrdCenter = new Point();
 
 var draggingKey;
 var draggingLastPos;
@@ -55,9 +59,9 @@ initialize();
 function mousedownListener(e) {
   if(draggingKey === undefined) {
     var mousePos = getMousePos(e);
-    for(var key in triangleCoords) {
-      var coords = convertAbsToRelPoint(triangleCoords[key]);
-      if(dist(coords, mousePos) < nodeRadius) {
+    for(var key in verts) {
+      var coords = convertAbsToRelPoint(verts[key]);
+      if(dist(coords, mousePos) < vertRadius + tolerance) {
         draggingKey = key;
         draggingLastPos = mousePos.clone();
         return;
@@ -92,27 +96,40 @@ function getMousePos(e) {
 }
 
 function updateTriangleVertex(change, key) {
-  triangleCoords[key].x += change.x;
-  triangleCoords[key].y += change.y;
+  verts[key].x += change.x;
+  verts[key].y += change.y;
 }
 
 function updateVars() {
-  var m1 = (triangleCoords.c.x - triangleCoords.a.x) / (triangleCoords.a.y - triangleCoords.c.y);
-  var m2 = (triangleCoords.c.x - triangleCoords.b.x) / (triangleCoords.b.y - triangleCoords.c.y);
+  // ORTHOCENTER
+  var m1 = (verts.c.x - verts.a.x) / (verts.a.y - verts.c.y);
+  var m2 = (verts.c.x - verts.b.x) / (verts.b.y - verts.c.y);
   if(!isFinite(m1) && !isFinite(m2)) {
-    // Weird Case
+    // Straight Line
     orthoCenter.x = x_bounds.x - 1;
     orthoCenter.y = y_bounds.x - 1;
   } else if(!isFinite(m1)) {
-    orthoCenter.x = triangleCoords.b.x;
-    orthoCenter.y = m2 * (orthoCenter.x - triangleCoords.a.x) + triangleCoords.a.y;
+    orthoCenter.x = verts.b.x;
+    orthoCenter.y = m2 * (orthoCenter.x - verts.a.x) + verts.a.y;
   } else if(!isFinite(m2)) {
-    orthoCenter.x = triangleCoords.a.x;
-    orthoCenter.y = m1 * (orthoCenter.x - triangleCoords.b.x) + triangleCoords.b.y;
+    orthoCenter.x = verts.a.x;
+    orthoCenter.y = m1 * (orthoCenter.x - verts.b.x) + verts.b.y;
   } else {
-    orthoCenter.x = (m1 * triangleCoords.b.x - m2 * triangleCoords.a.x + triangleCoords.a.y - triangleCoords.b.y) / (m1 - m2);
-    orthoCenter.y = m1 * (orthoCenter.x - triangleCoords.b.x) + triangleCoords.b.y;
+    orthoCenter.x = (m1 * verts.b.x - m2 * verts.a.x + verts.a.y - verts.b.y) / (m1 - m2);
+    orthoCenter.y = m1 * (orthoCenter.x - verts.b.x) + verts.b.y;
   }
+
+  // CIRCUMCENTER
+  var a_mag = verts.a.x * verts.a.x + verts.a.y * verts.a.y;
+  var b_mag = verts.b.x * verts.b.x + verts.b.y * verts.b.y;
+  var c_mag = verts.c.x * verts.c.x + verts.c.y * verts.c.y;
+  var d = 2 * (verts.a.x * (verts.b.y - verts.c.y) + verts.b.x * (verts.c.y - verts.a.y) + verts.c.x * (verts.a.y - verts.b.y));
+  circmCenter.x = (a_mag * (verts.b.y - verts.c.y) + b_mag * (verts.c.y - verts.a.y) + c_mag * (verts.a.y - verts.b.y)) / d;
+  circmCenter.y = (a_mag * (verts.c.x - verts.b.x) + b_mag * (verts.a.x - verts.c.x) + c_mag * (verts.b.x - verts.a.x)) / d;
+
+  // CENTROID
+  cntrdCenter.x = (verts.a.x + verts.b.x + verts.c.x) / 3;
+  cntrdCenter.y = (verts.a.y + verts.b.y + verts.c.y) / 3;
 }
 
 function drawCanvas() {
@@ -124,34 +141,25 @@ function drawCanvas() {
 
 function drawTriangle() {
   var coords = [];
-  coords.push(convertAbsToRelPoint(triangleCoords.a));
-  coords.push(convertAbsToRelPoint(triangleCoords.b));
-  coords.push(convertAbsToRelPoint(triangleCoords.c));
+  coords.push(verts.a);
+  coords.push(verts.b);
+  coords.push(verts.c);
 
-  context.strokeStyle = "#90C3D4";
   for(var i = 0; i < 3; i++) {
-    context.beginPath();
-    context.moveTo(coords[i].x, coords[i].y);
-    context.lineTo(coords[(i + 1) % 3].x, coords[(i + 1) % 3].y);
-    context.closePath();
-    context.stroke();
+    drawLine(coords[i], coords[(i + 1) % 3], 3, "#90C3D4");
+    drawLine(coords[i], orthoCenter, 1, "#A1D490");
+    drawLine(coords[i], circmCenter, 1, "#D4A190");
+    drawLine(coords[i], cntrdCenter, 1, "#C390D4");
   }
-  context.fillStyle = "#90C3D4";
   for(var i = 0; i < 3; i++) {
-    context.beginPath();
-    context.arc(coords[i].x, coords[i].y, nodeRadius, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
+    drawCircle(coords[i], vertRadius, "#90C3D4");
   }
 }
 
 function drawTriangleCenters() {
-  context.fillStyle = "#A1D490";
-  var coords = convertAbsToRelPoint(orthoCenter);
-  context.beginPath();
-  context.arc(coords.x, coords.y, 5, 0, 2 * Math.PI);
-  context.closePath();
-  context.fill();
+  drawCircle(orthoCenter, nodeRadius, "#A1D490");
+  drawCircle(circmCenter, nodeRadius, "#D4A190");
+  drawCircle(cntrdCenter, nodeRadius, "#C390D4");
 }
 
 function convertAbsToRelPoint(point) {
@@ -166,4 +174,25 @@ function convertRelToAbsPoint(point) {
   var y = point.y / canvas.height * y_height * -1.0;
 
   return new Point(x, y);
+}
+
+function drawLine(p1, p2, width, color) {
+  var conv_p1 = convertAbsToRelPoint(p1);
+  var conv_p2 = convertAbsToRelPoint(p2);
+  context.strokeStyle = color;
+  context.lineWidth = width;
+  context.beginPath();
+  context.moveTo(conv_p1.x, conv_p1.y);
+  context.lineTo(conv_p2.x, conv_p2.y);
+  context.closePath();
+  context.stroke();
+}
+
+function drawCircle(point, radius, color) {
+  context.fillStyle = color;
+  var conv_point = convertAbsToRelPoint(point);
+  context.beginPath();
+  context.arc(conv_point.x, conv_point.y, radius, 0, 2 * Math.PI);
+  context.closePath();
+  context.fill();
 }
